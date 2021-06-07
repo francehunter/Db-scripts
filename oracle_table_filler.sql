261eb756dcf5d8253643072604d37bf962632e92
Ilya <32356283+francehunter@users.noreply.github.com>
2021-06-07 11:17:47 +0300

    Update oracle_table_filler.sql

--- oracle_table_filler.sql
+++ oracle_table_filler.sql
@@ -1,14 +1,18 @@
 DECLARE
    curId NUMBER;
+   cnt NUMBER;
    stamp TIMESTAMP;
+   fname VARCHAR2(20);
    
 BEGIN 
     SELECT NVL(max(id),0) INTO curId FROM tablea;
     
     FOR x IN 1 .. 1000 LOOP
-        curId := curId + x;
-        stamp := CURRENT_TIMESTAMP;
+        curId := curId + 1;
+        select current_timestamp + numToDSInterval( dbms_random.value(1, 100*24*60*60), 'second' ) INTO stamp from dual;        
+        fname := DBMS_RANDOM.string('x',20);
+        cnt := DBMS_RANDOM.RANDOM();
         
-        INSERT INTO tablea (id,name,stamp) VALUES(curId,'lol',stamp);
+        INSERT INTO tablea (id,name,stamp,cnt) VALUES(curId,fname,stamp,cnt);
     END LOOP;
 END;
